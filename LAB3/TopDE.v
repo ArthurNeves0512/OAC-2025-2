0fa37097fe9610359b5e602b04a367d5faf2374b
Maria Fernanda <mfernanda.trrs@gmail.com>
2025-11-19 15:57:06 -0300

    Add files via upload

--- LAB3/TopDE.v
+++ LAB3/TopDE.v
@@ -1,1 +1,32 @@
+`ifndef PARAM
+	`include "Parametros.v"
+`endif
 
+module TopDE (
+	input logic CLOCK, Reset,
+	input logic [4:0] Regin,
+	output logic ClockDIV,
+	output logic [31:0] PC,Instr,Regout,
+	output logic [3:0] Estado
+	);
+	
+	// Corrigido para sincronismo e inicialização
+	always @(posedge CLOCK or posedge Reset) 
+	begin
+		if(Reset)
+            ClockDIV <= 1'b0; // FIX: Garante que o clock divisível comece em zero
+        else
+			ClockDIV <= ~ClockDIV;
+	end
+	
+/* Uniciclo UNI1 (.clockCPU(ClockDIV), .clockMem(CLOCK), .reset(Reset), 
+						.PC(PC), .Instr(Instr), .regin(Regin), .regout(Regout)); */
+
+	// Instância do Processador Multiciclo
+	Multiciclo MULT1 (.clockCPU(ClockDIV), .clockMem(CLOCK), .reset(Reset), 
+						.PC(PC), .Instr(Instr), .regin(Regin), .regout(Regout), .estado(Estado));	
+						
+/* Pipeline PIP1 (.clockCPU(ClockDIV), .clockMem(CLOCK), .reset(Reset), 
+						.PC(PC), .Instr(Instr), .regin(Regin), .regout(Regout)); */
+		
+endmodule
